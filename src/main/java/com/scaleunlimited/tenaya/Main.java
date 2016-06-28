f08966b75346758d3e8d63b0fcd942cdc593b525
Ryan Brott <rcbrott@gmail.com>
2016-06-28 00:02:15 -0700

    Add script to aggregate SRA data given an experiment

--- src/main/java/com/scaleunlimited/tenaya/Main.java
+++ src/main/java/com/scaleunlimited/tenaya/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		if (args.length > 0) {
 			String action = args[0].toLowerCase();
 			String[] modifiedArgs = Arrays.copyOfRange(args, 1, args.length);
@@ -12,6 +12,8 @@ public class Main {
 				SignatureComparisonTool.main(modifiedArgs);
 			} else if (action.equals("generate")) {
 				SignatureGenerationTool.main(modifiedArgs);
+			} else if (action.equals("fetch")) {
+				DataFetchTool.main(modifiedArgs);
 			} else {
 				System.err.println("Tool failed: unknown action '" + action + "'");
 			}
