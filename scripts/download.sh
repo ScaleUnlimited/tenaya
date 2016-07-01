5807cb2b9d360c540b9c288198e8cb117d990d0d
Ryan Brott <rcbrott@gmail.com>
2016-07-01 11:06:43 -0700

    Removed certificate dependency from the download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -8,19 +8,36 @@ fi
 
 # tmp=$TENAYA_HOME/tmp;
 cache=$TENAYA_HOME/cache;
-statuses=$TENAYA_HOME/statuses;
+logs=$TENAYA_HOME/logs;
 
 if [ -z $1 ]; then
-	output=$(realpath .)/fasta;
+	output=$(realpath .)/fasta_data;
 else
 	output=$1;
 fi
 
 # mkdir -p $tmp;
 mkdir -p $cache;
-mkdir -p $statuses;
-mkdir -p $output;
+mkdir -p $logs;
 # rm -rf $tmp/*;
+
+
+certpath=$TENAYA_HOME/cert.openssh;
+
+cat > $certpath <<- AsperaCertificate
+-----BEGIN DSA PRIVATE KEY-----
+MIIBuwIBAAKBgQDkKQHD6m4yIxgjsey6Pny46acZXERsJHy54p/BqXIyYkVOAkEp
+KgvT3qTTNmykWWw4ovOP1+Di1c/2FpYcllcTphkWcS8lA7j012mUEecXavXjPPG0
+i3t5vtB8xLy33kQ3e9v9/Lwh0xcRfua0d5UfFwopBIAXvJAr3B6raps8+QIVALws
+yeqsx3EolCaCVXJf+61ceJppAoGAPoPtEP4yzHG2XtcxCfXab4u9zE6wPz4ePJt0
+UTn3fUvnQmJT7i0KVCRr3g2H2OZMWF12y0jUq8QBuZ2so3CHee7W1VmAdbN7Fxc+
+cyV9nE6zURqAaPyt2bE+rgM1pP6LQUYxgD3xKdv1ZG+kDIDEf6U3onjcKbmA6ckx
+T6GavoACgYEAobapDv5p2foH+cG5K07sIFD9r0RD7uKJnlqjYAXzFc8U76wXKgu6
+WXup2ac0Co+RnZp7Hsa9G+E+iJ6poI9pOR08XTdPly4yDULNST4PwlfrbSFT9FVh
+zkWfpOvAUc8fkQAhZqv/PE6VhFQ8w03Z8GpqXx7b3NvBR+EfIx368KoCFEyfl0vH
+Ta7g6mGwIMXrdTQQ8fZs
+-----END DSA PRIVATE KEY-----
+AsperaCertificate
 
 i=0;
 while read run; do
@@ -42,12 +59,7 @@ for run in "${runs[@]}"; do
 	runpath=$cache/$run.sra;
 	if [ -z "$(echo "$list" | grep $run)" ]; then
 		if [ $log = true ]; then echo "Starting download into $runpath"; fi
-		certpath=$(realpath ./asperaweb_id_dsa.openssh);
-		if [ ! -f $certpath ]; then
-			echo "Tool failed: aspera certificate not found";
-			exit 1;
-		fi
-		ascp -i $certpath -k1 -Tr -l200m anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/${run:0:3}/${run:0:6}/${run}/${run}.sra $runpath > "$statuses/$run.log" &
+		ascp -i $certpath -k1 -Tr -l200m anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/${run:0:3}/${run:0:6}/${run}/${run}.sra $runpath > "$logs/$run.log" &
 	else
 		if [ $log = true ]; then echo "Found cached version at $runpath"; fi
 	fi
@@ -57,7 +69,7 @@ while true; do
   running=0;
 	i=0;
 	for run in "${runs[@]}"; do
-		line=$(cat "$statuses/$run.log");
+		line=$(cat "$logs/$run.log");
 		if [ "$(echo $line | grep Completed)" ]; then
 			if [ $log = true ]; then echo -ne "$run: Done"; fi
 		elif [ "$(echo "$line" | grep skipped)" ]; then
@@ -81,6 +93,8 @@ while true; do
 done
 
 if [ $log = true ]; then echo "All downloads complete"; fi
+
+mkdir -p $output;
 
 for run in "${runs[@]}"; do
 	runpath=$cache/$run.sra;
